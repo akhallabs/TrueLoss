29c29748edef6d8f133db86064b0588f80126414
TrueLoss <trueloss@tm.true.loss>
2026-08-30 22:54:08 +0300

    fix: core:domain missing kotlinx-coroutines-core for FlowUseCase

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
 android { namespace = "tm.trueloss.core.domain" }
 dependencies {
     implementation(project(":core:common"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
 }
