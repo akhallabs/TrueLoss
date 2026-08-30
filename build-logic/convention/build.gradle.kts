0e613396bbab8dc3ae2efdb9d89ce67706e2a82b
TrueLoss <trueloss@tm.true.loss>
2026-08-30 21:44:53 +0300

    fix: register convention plugins via gradlePlugin block - tm.trueloss.* not found

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -10,3 +10,39 @@ dependencies {
     implementation("com.android.tools.build:gradle:8.7.3")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
 }
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = "tm.trueloss.android.application"
+            implementationClass = "tm.trueloss.AndroidApplicationConventionPlugin"
+        }
+        register("androidLibrary") {
+            id = "tm.trueloss.android.library"
+            implementationClass = "tm.trueloss.AndroidLibraryConventionPlugin"
+        }
+        register("androidCompose") {
+            id = "tm.trueloss.android.compose"
+            implementationClass = "tm.trueloss.AndroidComposeConventionPlugin"
+        }
+        register("androidHilt") {
+            id = "tm.trueloss.android.hilt"
+            implementationClass = "tm.trueloss.AndroidHiltConventionPlugin"
+        }
+        register("androidRoom") {
+            id = "tm.trueloss.android.room"
+            implementationClass = "tm.trueloss.AndroidRoomConventionPlugin"
+        }
+        register("featureDomain") {
+            id = "tm.trueloss.feature.domain"
+            implementationClass = "tm.trueloss.FeatureDomainConventionPlugin"
+        }
+        register("featureData") {
+            id = "tm.trueloss.feature.data"
+            implementationClass = "tm.trueloss.FeatureDataConventionPlugin"
+        }
+        register("featureUi") {
+            id = "tm.trueloss.feature.ui"
+            implementationClass = "tm.trueloss.FeatureUiConventionPlugin"
+        }
+    }
+}
