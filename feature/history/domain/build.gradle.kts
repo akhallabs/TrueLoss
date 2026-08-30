62bdf5db3b635d049e0307014bea723c69f86590
TrueLoss <trueloss@tm.true.loss>
2026-08-30 23:06:54 +0300

    fix: all feature:domain missing coroutines + javax.inject for Flow and @Inject

--- feature/history/domain/build.gradle.kts
+++ feature/history/domain/build.gradle.kts
@@ -6,4 +6,6 @@ android { namespace = "tm.trueloss.feature.history.domain" }
 dependencies {
     implementation(project(":core:common"))
     implementation(project(":core:domain"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    implementation("javax.inject:javax.inject:1")
 }
--- feature/result/domain/build.gradle.kts
+++ feature/result/domain/build.gradle.kts
@@ -1,3 +1,8 @@
 plugins { alias(libs.plugins.kotlin.android); id("tm.trueloss.android.library") }
 android { namespace = "tm.trueloss.feature.result.domain" }
-dependencies { implementation(project(":core:common")); implementation(project(":core:domain")) }
+dependencies {
+    implementation(project(":core:common"))
+    implementation(project(":core:domain"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    implementation("javax.inject:javax.inject:1")
+}
