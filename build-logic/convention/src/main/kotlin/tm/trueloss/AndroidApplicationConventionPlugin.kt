74e66b61404e87f769e58f864253dee56a02bf4b
TrueLoss <trueloss@tm.true.loss>
2026-08-30 21:47:40 +0300

    fix: JavaVersion import + remove duplicate META-INF gradle-plugins (gradlePlugin auto-generates)

--- build-logic/convention/src/main/kotlin/tm/trueloss/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/tm/trueloss/AndroidApplicationConventionPlugin.kt
@@ -1,5 +1,6 @@
 package tm.trueloss
 import com.android.build.api.dsl.ApplicationExtension
+import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
